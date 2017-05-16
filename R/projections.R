783478810e1233b164205f4f5a8781d9530523ad
Pierre Nouvellet <P.nouvellet@imperial.ac.uk>
2017-05-16 17:33:15 +0100

    first projection
    first stab at producing projections + check si_dist contain a 'shape'
    and 'scale'

--- R/projections.R
+++ R/projections.R
@@ -25,21 +25,52 @@
 
 project <- function(x, R, si, n_sim = 100, n_days = 7) {
 
-    ## Various checks on inputs
-    
-    if (!inherits(x, "incidence")) {
-        msg <- "x is not an incidence object"
-        stop(msg)
-    }
+  ## Various checks on inputs
+  
+  if (!inherits(x, "incidence")) {
+    msg <- "x is not an incidence object"
+    stop(msg)
+  }
+  
+  if (as.integer(x$interval) != 1L) {
+    msg <- sprintf("daily incidence needed (current interval is %d days)",
+                   x$interval)
+    stop(msg)
+  }
+  
+  if (names(si_dist)=='shape' != 1) {
+    msg <- sprintf("si_dist does not contain a shape)",
+                   si_dist)
+    stop(msg)
+  }
+  
+  if (names(si_dist)=='scale' != 1) {
+    msg <- sprintf("si_dist does not contain a scale)",
+                   si_dist)
+    stop(msg)
+  }
+  
+  ## number of days with observations
+  t_prior <- length(x)
+  
+  ## serial interval and its reversed
+  si <- distcrete("gamma", interval = 1L, shape = si_dist$shape, scale = si_dist$scale)$d
+  ws <- rev(si(0:(n_days+t_prior-1)))
+  
+  ## initial conditions
+  I0 <- x%*%matrix(1,1,n_sim)
+  
+  ## projection
+  I_proj <- rbind(I0,matrix(0,n_days,n_sim))
+  for (i in (t_prior+1):(t_prior+n_days)){
+    lambda <- tail(ws,i) %*% I_new[1:i,]
+    I_proj[i,] <- rpois(n_sim,R*lambda)
+  }
+  # store simulations
+  I_proj <- I_proj[(t_prior+1):(t_prior+n_days),]
+  
+  
+  return(I_proj)
 
-    if (as.integer(x$interval) != 1L) {
-        msg <- sprintf("daily incidence needed (current interval is %d days)",
-                       x$interval)
-        stop(msg)
-    }
-
-
-
-    
 }
 
