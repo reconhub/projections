a99c1535172e0936b64ab7943eded07cd2e94aaf
Thibaut Jombart <thibautjombart@gmail.com>
2017-12-05 11:48:57 +0000

    adding conversion

---
+++ tests/testthat/test-conversion.R
@@ -0,0 +1,24 @@
+context("Test conversion")
+
+test_that("Test against reference results", {
+    skip_on_cran()
+
+    ## simulate basic epicurve
+    dat <- c(0, 2, 2, 3, 3, 5, 5, 5, 6, 6, 6, 6)
+    i <- incidence(dat)
+
+
+    ## example with a function for SI
+    si <- distcrete("gamma", interval = 1L,
+                    shape = 1.5,
+                    scale = 2, w = 0)
+
+    set.seed(1)
+    pred_1 <- project(i, runif(100, 0.8, 1.9), si, n_days = 30)
+    df_1 <- as.data.frame(pred_1)
+    expect_equal_to_reference(df_1, file = "rds/df_1.rds")
+    expect_equal(as.vector(unlist(df_1[, -1])), as.vector(pred_1))
+    
+})
+
+
