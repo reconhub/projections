8a22910bdd3de915ed7df076472132d8ec54d9cf
Thibaut Jombart <thibautjombart@gmail.com>
2017-12-05 11:53:49 +0000

    adding internal tests

---
+++ tests/testthat/test-internals.R
@@ -0,0 +1,23 @@
+context("Test internals")
+
+test_that("Errors are thrown when they should", {
+    expect_error(projections:::assert_reporting("asasd"),
+                 "reporting is not numeric")
+    
+    expect_error(projections:::assert_reporting(Inf),
+                 "reporting is not a finite value")
+
+    expect_error(projections:::assert_reporting(NA_integer_),
+                 "reporting is not a finite value")
+    
+    expect_error(projections:::assert_reporting(0),
+                 "reporting <= 0")
+    
+    expect_error(projections:::assert_reporting(-123),
+                 "reporting <= 0")
+    
+    expect_error(projections:::assert_reporting(1.1),
+                 "reporting > 1")
+    
+})
+
