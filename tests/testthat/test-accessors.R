71c47ca9068d610a0da913455e15706f2092553f
Thibaut Jombart <thibautjombart@gmail.com>
2017-12-05 11:44:34 +0000

    adding accessors

---
+++ tests/testthat/test-accessors.R
@@ -0,0 +1,21 @@
+context("Test accessors")
+
+test_that("Accessors return the right thing", {
+    skip_on_cran()
+
+    ## simulate basic epicurve
+    dat <- c(0, 2, 2, 3, 3, 5, 5, 5, 6, 6, 6, 6)
+    i <- incidence(dat)
+
+    ## example with a function for SI
+    si <- distcrete("gamma", interval = 1L,
+                    shape = 1.5,
+                    scale = 2, w = 0)
+
+  
+    pred_1 <- project(i, runif(100, 0.8, 1.9), si, n_days = 30)
+    expect_equal(attr(pred_1, "dates"), get_dates(pred_1))
+    expect_equal(i, get_incidence(pred_1))
+
+})
+
