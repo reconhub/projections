38bc56e6366416476bb8b9529bb480d921ae091e
Thibaut Jombart <thibautjombart@gmail.com>
2017-12-05 11:30:34 +0000

    adding tests

---
+++ tests/testthat/test-project.R
@@ -0,0 +1,46 @@
+context("Test project function")
+
+test_that("Test against reference results", {
+    skip_on_cran()
+
+    ## simulate basic epicurve
+    dat <- c(0, 2, 2, 3, 3, 5, 5, 5, 6, 6, 6, 6)
+    i <- incidence(dat)
+
+
+    ## example with a function for SI
+    si <- distcrete("gamma", interval = 1L,
+                    shape = 1.5,
+                    scale = 2, w = 0)
+
+    set.seed(1)
+    pred_1 <- project(i, runif(100, 0.8, 1.9), si, n_days = 30)
+    expect_equal_to_reference(pred_1, file = "rds/pred_1.rds")
+
+})
+
+
+
+
+
+test_that("Test that dates start when needed", {
+    skip_on_cran()
+
+    ## simulate basic epicurve
+    dat <- c(0, 2, 2, 3, 3, 5, 5, 5, 6, 6, 6, 6)
+    i <- incidence(dat)
+
+
+    ## example with a function for SI
+    si <- distcrete("gamma", interval = 1L,
+                    shape = 1.5,
+                    scale = 2, w = 0)
+
+    set.seed(1)
+    pred_1 <- project(i, runif(100, 0.8, 1.9), si, n_days = 30)
+    expect_equal(max(i$dates) + 1, attr(pred_1, "dates"))
+
+})
+
+
+
