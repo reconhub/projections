94022f6c9f2b605515a8bbbfd7a88cdece69a301
Thibaut <thibautjombart@gmail.com>
2020-03-06 16:16:06 +0000

    more tests

--- tests/testthat/test-build_projections.R
+++ tests/testthat/test-build_projections.R
@@ -76,9 +76,15 @@ test_that("Test ordering", {
     mat <- matrix(round(rnorm(100, 10)), ncol = 20)
     dates <- Sys.Date() + c(5, 1, 3, 2, 4)
 
+    ## test ordering
     x <- build_projections(mat, dates)
-
     expect_identical(get_dates(x), sort(dates))
-    expect_equal(as.vector(mat[order(dates), ]), as.vector(x))  
+    expect_equal(as.vector(mat[order(dates), ]), as.vector(x))
+
+    ## test no ordering
+    x <- build_projections(mat, dates, order_dates = FALSE)
+    expect_identical(get_dates(x), dates)
+    expect_equal(as.vector(mat), as.vector(x))
+    
 })
 
