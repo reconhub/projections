3be2f7b3460805647e89c8f0768e72c6cf6d6d74
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2020-12-10 15:58:23 +0000

    make plot checks conditional on svglite

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,12 +1,17 @@
-library(svglite)
 save_svg <- function(plot) {
   path <- tempfile(fileext = ".svg")
   ggplot2::ggsave(path, plot = plot)
   path
 }
 
+expect_snapshot_plot <- function(code, name) {
+  # Other packages might affect results
+  skip_if_not_installed("svglite")
+  path <- save_svg(code)
+  expect_snapshot_file(path, paste0(name, ".svg"))
+}
+
 test_that("Test against reference results", {
-  skip_on_cran()
 
   ## simulate basic epicurve
   dat <- c(0, 2, 2, 3, 3, 5, 5, 5, 6, 6, 6, 6)
@@ -22,7 +27,7 @@ test_that("Test against reference results", {
   pred_1 <- project(i, runif(100, 0.8, 1.9), si, n_days = 30)
   plot_1 <- plot(pred_1)
   
-  expect_snapshot_file(save_svg(plot_1), "basic-example-plot.svg")
+  expect_snapshot_plot(plot_1, "basic-example-plot")
     
   ## using simulated ebola data
 
@@ -41,28 +46,28 @@ test_that("Test against reference results", {
 
   ## plotting projections
   plot_2 <- plot(proj)
-  expect_snapshot_file(save_svg(plot_2), "evd-proj.svg")
+  expect_snapshot_plot(plot_2, "evd-proj")
 
   plot_3 <- plot(proj, boxplots = TRUE, outliers = FALSE)
-  expect_snapshot_file(save_svg(plot_3), "evd-proj-box-no-outliers.svg")
+  expect_snapshot_plot(plot_3, "evd-proj-box-no-outliers")
 
   plot_4 <- plot(proj, ribbon = FALSE)
-  expect_snapshot_file(save_svg(plot_4), "evd-proj-no-ribbon.svg")
+  expect_snapshot_plot(plot_4, "evd-proj-no-ribbon")
 
   plot_5 <- plot(proj, boxplots = FALSE, linetype = 2, linesize = 3)
-  expect_snapshot_file(save_svg(plot_5), "evd-proj-no-box-custom-lines.svg")
+  expect_snapshot_plot(plot_5, "evd-proj-no-box-custom-lines")
   
   plot_6 <- plot(proj, boxplots = TRUE, boxplots_color = "red")
-  expect_snapshot_file(save_svg(plot_6), "evd-proj-red-box.svg")
+  expect_snapshot_plot(plot_6, "evd-proj-red-box")
 
   plot_7 <- plot(proj, quantiles = FALSE, ribbon = FALSE, boxplots = TRUE)
-  expect_snapshot_file(save_svg(plot_7), "evd-proj-box-only.svg")
+  expect_snapshot_plot(plot_7, "evd-proj-box-only")
 
   plot_8 <- plot(proj, quantiles = FALSE)
-  expect_snapshot_file(save_svg(plot_8), "evd-proj-ribbon-only.svg")
+  expect_snapshot_plot(plot_8, "evd-proj-ribbon-only")
 
   plot_9 <- plot(proj, ribbon_color = "red", quantiles = FALSE)
-  expect_snapshot_file(save_svg(plot_9), "evd-proj-red-ribbon.svg")
+  expect_snapshot_plot(plot_9, "evd-proj-red-ribbon")
 
   plot_10 <- plot(
     proj, 
@@ -71,20 +76,20 @@ test_that("Test against reference results", {
     quantiles = FALSE,
     ribbon_quantiles = c(.4, .6)
   )    
-  expect_snapshot_file(save_svg(plot_10), "evd-proj-full-red-ribbon-narrow-range.svg")
+  expect_snapshot_plot(plot_10, "evd-proj-full-red-ribbon-narrow-range")
      
   
   ## adding projections to incidence::incidence plot
   plot_11 <- plot(i) %>% add_projections(proj)
-  expect_snapshot_file(save_svg(plot_11), "evd-proj-with-incidence-incidence.svg")
+  expect_snapshot_plot(plot_11, "evd-proj-with-incidence-incidence")
   
   plot_12 <- plot(i) %>% add_projections(proj, boxplots = TRUE)
-  expect_snapshot_file(save_svg(plot_12), "evd-proj-with-incidence-incidence-no-box.svg")
+  expect_snapshot_plot(plot_12, "evd-proj-with-incidence-incidence-no-box")
   
   plot_13 <- 
     plot(i) %>% 
     add_projections(proj, quantiles = FALSE, ribbon = FALSE, boxplots = TRUE)
-  expect_snapshot_file(save_svg(plot_13), "evd-proj-with-incidence-incidence-box-only.svg")
+  expect_snapshot_plot(plot_13, "evd-proj-with-incidence-incidence-box-only")
   
   
   ## same, custom colors and quantiles
@@ -93,13 +98,9 @@ test_that("Test against reference results", {
   plot_14 <- plot(i[1:200]) %>%
     add_projections(proj, quantiles, palette = pal) +
     ggplot2::scale_x_date(date_labels = "%b %Y")
-  expect_snapshot_file(save_svg(plot_14), "evd-proj-with-incidence-incidence-and-custom.svg")
+  expect_snapshot_plot(plot_14, "evd-proj-with-incidence-incidence-and-custom")
 
 })
-
-
-
-
 
 test_that("Plotting issues expected errors", {
   skip_on_cran()
